cd1c269a9988f552e44b535bc2b646af3dcdc30d
yjlee9909 <yjlee9909@naver.com>
2022-11-05 17:15:24 +0900

    벤딩머신 JS

--- vendingmachine/src/js/components/vendingmachine.js
+++ vendingmachine/src/js/components/vendingmachine.js
@@ -22,7 +22,17 @@ class Vendingmachine {
     stagedItemGenerator(target) {
         const stagedItem = document.createElement('li');
         stagedItem.dataset.item = target.dataset.item;
+        stagedItem.dataset.price = target.dataset.price;
+        stagedItem.innerHTML = `
+    <button type="button" class="btn-staged">
+            <img src="./src/images/${target.dataset.img}" alt="" class="img-item">
+            <strong class="txt-item">${target.dataset.item}</strong>
+            <span class="num-counter">1</span>
+            </button>
+        `;
+        this.stagedList.appendChild(stagedItem);
     }
+
 
     bindEvents() {
         /*
@@ -100,10 +110,30 @@ class Vendingmachine {
                     // 잔액
                     this.balance.textContent = new Intl.NumberFormat().format(balanceVal - targetElPrice) + '원';
 
+                    // forEach 문을 사용할 경우 반복의 종료가 불가능하다(return, break 작동 안함). 모든 원소를 순환할 필요가 없다면 비효율적.
+                    for (const item of stagedListItem) {  // 클릭한 음료수가 내가 이미 선택한 아이템인지 탐색
+                        if (item.dataset.item === targetEl.dataset.item) { //내가 클릭한 상품과 내가 담은 상품이 같을 경우
+                            item.querySelector('.num-counter').textContent++;
+                            isStaged = true;
+                            break;
+                        }
+                    };
+                    // 해당 아이템을 처음 선택했을 경우
                     // 클릭한 아이템 생성하기
                     // false -> true 선택되었을 때
                     if (!isStaged) {
                         this.stagedItemGenerator(targetEl);
+                    }
+                    // 콜라의 갯수를 줄입니다.
+                    targetEl.dataset.count--;
+
+                    if (parseInt(targetEl.dataset.count) === 0) { // 상품이 소진되면 품절 표시
+                        targetEl.parentElement.classList.add('sold-out');
+                        const warning = document.createElement('em');
+                        warning.textContent = '해당상품은 품절입니다.';
+                        warning.classList.add('ir');
+                        // em 요소를 button 요소의 앞에 배치합니다.
+                        targetEl.parentElement.insertBefore(warning, targetEl);
                     }
                     // 클릭한 아이템이 이미 존재하는지 비교
                 } else {
@@ -111,6 +141,43 @@ class Vendingmachine {
                 }
             })
         })
+
+        /**
+         * 4. 획득 버튼 기능
+         * 획득 버튼을 누르면 선택한 음료수 목록이 획득한 음료 목록으로 이동합니다.
+         * 획득한 음료의 금액을 모두 합하여 총금액을 업데이트 합니다.
+        */
+        this.btnGet.addEventListener('click', (event) => {
+            let isGot = false;
+            let totalPrice = 0;
+
+            // 내가 고른 음료수 목록과 이미 구입한 목록을 비교
+            for (const itemStaged of this.stagedList.querySelectorAll('li')) {
+                for (const itemGot of this.gotList.querySelectorAll('li')) {
+                    let itemGotCount = itemGot.querySelector('.num-counter');
+                    // 획득할 아이템이 이미 획득한 음료 리스트에 존재하는지 확인
+                    if (itemStaged.dataset.item === itemGot.dataset.item) {
+                        //획득한 음료 리스트의 아이템 갯수 업데이트 
+                        itemGotCount.textContent = parseInt(itemGotCount.textContent) + parseInt(itemStaged.querySelector('.num-counter').textContent);
+                        isGot = true;
+                        break;
+                    }
+                }
+                // 처음 획득하는 음료수라면
+                if (!isGot) {
+                    this.gotList.appendChild(itemStaged);
+                }
+            }
+
+            // stagedList 목록의 내용을 초기화
+            this.stagedList.innerHTML = null;
+
+            // 획득한 음료 리스트를 순환하면서 총 금액을 계산합니다.
+            this.gotList.querySelectorAll('li').forEach((itemGot) => {
+                totalPrice += itemGot.dataset.price * parseInt(itemGot.querySelector('.num-counter').textContent);
+            });
+            this.txtTotal.textContent = `총금액 : ${new Intl.NumberFormat().format(totalPrice)}원`;
+        });
 
     }
 }
--- vendingmachine/src/js/index.js
+++ vendingmachine/src/js/index.js
@@ -6,8 +6,14 @@ const colaGenerator = new ColaGenerator();
 const vendingmachine = new Vendingmachine();
 
 
-// await - 기다림 top level에서 작동함
-// 대상은 async여야 함 setup메서드 자체가 async여야 await로 사용 가능
+// 자바스크립트는 await를 만나면 프라미스 객체가 반환될때까지 기다립니다.
+// 원래 await는 탑레벨에서 작동하지 않습니다. https://ko.javascript.info/async-await
+// (async () => {
+//     await colaGenerator.setup();
+//     vendingmachineFunc.setup()
+
+// })()
+
+// 탑레벨 await는 최상위 모듈에서만 작동하며, async 함수만 사용할 수 있습니다. https://v8.dev/features/top-level-await
 await colaGenerator.setup();
 vendingmachine.setup();
-
