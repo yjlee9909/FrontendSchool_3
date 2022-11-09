262c4b66523aeb16caf4adeaed2baa61c60c781e
yjlee9909 <yjlee9909@naver.com>
2022-11-09 21:04:36 +0900

    11월 9일 Node.js

---
+++ Node/003.js
@@ -0,0 +1,6 @@
+
+const 이름 = 'lee';
+const 나이 = 10;
+
+module.exports.이름 = 이름;
+module.exports.나이 = 나이;
---
+++ Node/004.js
@@ -0,0 +1,9 @@
+// 그나마 최신 문법
+
+import * as 개인프로필 from './003.js';
+// const 개인프로필 = require('./003');
+
+console.log(개인프로필.이름);
+console.log(개인프로필.나이);
+
+// 설정 따로 필요함
