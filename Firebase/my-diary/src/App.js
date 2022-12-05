13ec2c6475f39800f76efc6d90e08b554cf1d809
yjlee9909 <yjlee9909@naver.com>
2022-12-05 17:02:06 +0900

    add: 12월 5일 파이어베이스

---
+++ Firebase/my-diary/src/App.js
@@ -0,0 +1,12 @@
+import {BrowserRouter} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App
